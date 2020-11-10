664cb21f70cd1f8b78a5442c30c6087580781ef8
Bartosz Wroblewski <wroobal@gmail.com>
2020-11-10 19:42:16 +0100

    Warsztat pierwszy. Gra w zgadywanie liczb

---
+++ guessing_numbers.py
@@ -0,0 +1,21 @@
+import random
+
+# draw a number
+drawn_number = random.randint(1, 100)
+
+while True:
+    # checking is a number is integer
+    try:
+        get_number = int(input("Guess the number: "))
+    except ValueError:
+        print("It's not a number!")
+    # checking that a number is bigger than drawn number
+    if drawn_number < get_number:
+        print("To big!")
+    # checking that a number is smaller than drawn number
+    elif drawn_number > get_number:
+        print("To small!")
+    # checking is that correct number
+    else:
+        print("You win !")
+        break
