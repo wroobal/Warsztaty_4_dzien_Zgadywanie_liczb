7a6198d1fe738c0c793af1f2b427b3b76f8872b1
Bartosz Wroblewski <wroobal@gmail.com>
2020-11-15 17:09:35 +0100

    poprawiona wersja

--- guessing_numbers.py
+++ guessing_numbers.py
@@ -7,15 +7,16 @@ while True:
     # checking is a number is integer
     try:
         get_number = int(input("Guess the number: "))
+
+        # checking that a number is bigger than drawn number
+        if drawn_number < get_number:
+            print("To big!")
+        # checking that a number is smaller than drawn number
+        elif drawn_number > get_number:
+            print("To small!")
+        # checking is that correct number
+        else:
+            print("You win !")
+            break
     except ValueError:
         print("It's not a number!")
-    # checking that a number is bigger than drawn number
-    if drawn_number < get_number:
-        print("To big!")
-    # checking that a number is smaller than drawn number
-    elif drawn_number > get_number:
-        print("To small!")
-    # checking is that correct number
-    else:
-        print("You win !")
-        break
